ab4a2849d6372c30469a03037a0721119d9c3e0b
lecopro123 <lecopro123@gmail.com>
2023-08-17 18:43:56 +0530

    17/08/23

--- is_2_2.py
+++ is_2_2.py
@@ -1,17 +1,15 @@
 # Importing the necessary modules
 import autograd.numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import multivariate_normal, uniform,norm
+from scipy.stats import multivariate_normal, uniform, norm
 from autograd import jacobian
 
-plt.style.use('seaborn-dark')
-plt.rcParams['figure.figsize'] = 14, 6
 
-n1=10000
-th=3.5
-mean_x=0
-var_x=1
-var_x_star = 1  
+n1 = 10000
+th = 3.5
+mean_x = 0
+var_x = 1
+var_x_star = 1
 mean_x_star = 3.5
 x = norm.rvs(loc=mean_x, scale=var_x, size=n1)
 x_star = norm.rvs(loc=mean_x_star, scale=var_x_star, size=n1)
@@ -19,21 +17,19 @@ x_star = norm.rvs(loc=mean_x_star, scale=var_x_star, size=n1)
 # Initializing the random seed
 random_seed = n1
 
-
 cov_val = 0  #[-0.8, 0, 0.8]
 
 mean = np.array([0, 0])
-
-
-
+mean_sam = np.array([0.87, 0.87])
 
 cov = np.array([[1, 0], [0, 1]])
-
-
+cov2 = np.array([[1, 0.5], [0.5, 2]])
 
 distr = multivariate_normal(cov=cov, mean=mean, seed=random_seed)
+distr_sam = multivariate_normal(cov=cov2, mean=mean_sam, seed=random_seed)
 
-
+sam = distr_sam.rvs(size=n1)
+#print(len(sam))
 data = distr.rvs(size=n1)
 
 a = np.log(1 + np.absolute(data))
@@ -41,7 +37,9 @@ a = np.log(1 + np.absolute(data))
 b = np.linalg.norm(np.log(1 + np.abs(data)), ord=np.inf)
 exp = a / b
 #print((5 / 1.8)**exp)
-new_rv = data * (3.5 / 1.848)**exp  #4 and 2.1(1k)   2.5 and 1.2(1k)  3.5and 1.88(1k)
+new_rv = data * (
+    3.5 /
+    1.848)**exp  #4 and 2.1(1k)   2.5 and 1.2(1k)  3.5and 1.88(1k) 1.848(10k)
 # new_rv_1 = data * (10 / 1.8)**exp
 
 
@@ -55,15 +53,14 @@ Jacobian = jacobian_matrix_function(data)
 wei = distr.pdf(new_rv) / distr.pdf(data)
 weight = np.matmul(wei, Jacobian)
 
-#print(weight)
-c1=[]
-x1=[]
-count = np.array([[0,0],[0,0]])
+print(Jacobian)
+c1 = []
+x1 = []
+count = np.array([[0, 0], [0, 0]])
 for i in range(1, n1):
-    for j in range(0, 2):
-        if new_rv[i][j] >= 3.5:
-            count = count + weight * new_rv[i][j]
-    a=count/i
+    if new_rv[i][0]**2 + new_rv[i][1]**2 >= 3.5:
+        count = count + weight*new_rv[i][0]
+    a = count / i
     #print(a)
     #print(np.linalg.det(a))
     c1.append(np.linalg.det(a))
@@ -71,31 +68,57 @@ for i in range(1, n1):
     #print(count/i)
 
 k = count / n1
-print(np. linalg. det(k))
+print("IS new app",np.linalg.det(k))
 
-count_mc= 0
+# count_mc = 0
+# c_m = []
+# x_m = []
+# for i in range(1, n1):  #1 for line 25
+#     if (x[i] > th):
+#         #print(x[i])
+#         count_mc = count_mc + 1
+#     c_m.append(count_mc / i)
+#     x_m.append(i)
+# print("MC", count_mc / n1)
+
+# count_mc = 0
+# c_m = []
+# x_m = []
+# for i in range(1, n1):  #1 for line 25
+#     a = np.random.normal(0, 1)
+#     if (a > th):
+#         #print(x[i])
+#         count_mc = count_mc + 1
+#     c_m.append(count_mc / i)
+#     x_m.append(i)
+# print("MC", count_mc / n1)
+
+count_mc = np.array([[0, 0], [0, 0]])
 c_m = []
 x_m = []
 for i in range(1, n1):  #1 for line 25
-    if (x[i] > th):
+    for j in range(0,2):
+        if (sam[i][j] > th):
         #print(x[i])
-        count_mc = count_mc + 1
-    c_m.append(count_mc / i)
+            count_mc = count_mc + np.array([[1, 0], [0, 1]])
+    c_m.append(np.linalg.det(count_mc / i))
     x_m.append(i)
-print("MC", count_mc / n1)
+print("MC", np.linalg.det(count_mc / n1))
 
-count_is = 0
+count_is = np.array([[0, 0], [0, 0]])
 c_is = []
 x_is = []
-for j in range(1, n1):  ##1 for line 36
-    if (x_star[j] > th):
-        #print(x_star[j])
-        count_is = count_is + (
-            (norm.pdf(x_star[j], loc=mean_x, scale=var_x) /
-             norm.pdf(x_star[j], loc=mean_x_star, scale=var_x_star)))
-    c_is.append(count_is / j)
-    x_is.append(j)
-print("MC+IS", count_is / n1)
+wei = distr.pdf(sam) / distr_sam.pdf(sam)
+#print(wei)
+for i in range(1, n1):  ##1 for line 36
+    for j in range(0, 2):
+        if (sam[i][j] > th):
+            #print(x_star[j])
+            count_is = count_is + weight
+            #print(distr.pdf(sam[i][j]) / distr_sam.pdf(sam[i][j]))
+        c_is.append(np.linalg.det(count_is / i))
+        x_is.append(i)
+print("MC+IS", np.linalg.det(count_is / n1))
 
 # Plotting the generated samples
 plt.figure()
@@ -132,18 +155,17 @@ plt.axis('equal')
 
 plt.show()
 
-
 true = []
 st = []
 for j in range(n1):
-    true.append(0.000235)
+    true.append(0.000229)
     st.append(j)
 
 plt.figure()
 plt.grid()
-plt.plot(x1,c1,label="IS new app")
-plt.plot(x_is,c_is,label="IS app")
-plt.plot(x_m,c_m,label="MC app")
+#plt.plot(x1, c1, label="IS new app")
+#plt.plot(x_is, c_is, label="IS app")
+plt.plot(x_m, c_m, label="MC app")
 plt.plot(st, true, label='True value')
 plt.xlabel('Number of iterations')
 plt.legend()
---
+++ is_2_2_new.py
@@ -0,0 +1,122 @@
+import autograd.numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal, uniform, norm
+from autograd import jacobian
+                                                 #####loss function : x^2+y^2>=3.5
+n1=10000000
+mean_x=0
+var_x=1
+sam_mean_x=2
+sam_var_x=2
+th=3.5
+x1 = norm.rvs(loc=mean_x, scale=var_x, size=n1)
+x2 = norm.rvs(loc=mean_x, scale=var_x, size=n1)
+sam_x1 = norm.rvs(loc=sam_mean_x, scale=sam_var_x, size=n1)
+sam_x2 = norm.rvs(loc=sam_mean_x, scale=sam_var_x, size=n1)
+
+###########################new app IS
+a1 = np.log(1 + np.absolute(x1))
+a2= np.log(1 + np.absolute(x2))
+b=np.log(1.88)
+exp2=a2/b
+exp1=a1/b
+new_x1 = x1 * (3.5 /3.11)**exp1  #2.8 10k  3.09 1l  3.11 1c
+new_x2 = x2 * (3.5 /2.44)**exp2  #2.5 10k  2.45 1l  2.44 1c
+
+# j1k=1+((np.absolute(x1)/(1 + np.absolute(x1)))*(np.log(3.5/1.88)/np.log(1.88)))
+# j2k=1+((np.absolute(x2)/(1 + np.absolute(x2)))*(np.log(3.5/1.88)/np.log(1.88)))
+# jac1=j1k*j2k*(3.5 /1.88)**(exp1)
+# jac2=j1k*j2k*(3.5 /1.88)**(exp2)
+# #jac=j1k*j2k*(3.5 /1.88)**(exp2+exp1)
+# weight1=(norm.pdf(new_x1,loc=np.mean(new_x1),scale=np.var(new_x1))*jac1)/(norm.pdf(x1,loc=np.mean(x1),scale=np.var(x1)))
+# weight2=(norm.pdf(new_x2,loc=np.mean(new_x2),scale=np.var(new_x2))*jac2)/(norm.pdf(x2,loc=np.mean(x2),scale=np.var(x2)))
+weight1=(norm.pdf(new_x1,loc=np.mean(x1),scale=np.var(x1)))/(norm.pdf(new_x1,loc=np.mean(new_x1),scale=np.var(new_x1)))  #weight of 1st dim
+weight2=(norm.pdf(new_x2,loc=np.mean(x2),scale=np.var(x2)))/(norm.pdf(new_x2,loc=np.mean(new_x2),scale=np.var(new_x2)))  #weight of 2nd dim
+
+count1=0
+#count2=0
+c1 = []
+x_1 = []
+
+for i in range(1,n1):   ###looping through the samples
+        if new_x1[i]**2 + new_x2[i]**2 >= 3.5:  ####checking acc to the 3rd step of Algo 1
+                # if weight1[i]>weight2[i]:
+                #     k=weight1[i]/(weight1[i]+weight2[i])
+                # elif weight1[i]<weight2[i]:
+                #     k=weight2[i]/(weight1[i]+weight2[i])
+                # else:
+                #     k=weight2[i]/(weight1[i]+weight2[i])
+                count1 = count1 + weight1[i]*weight2[i]
+                #count2 = count2 + weight2[i]
+        c1.append(count1/i)
+        x_1.append(i)
+print("IS new app",count1/n1)
+#print(count2/(n1))
+
+###########################vanilla MC
+count_mc=0
+c_m = []
+x_m = []
+#print(len(x1))
+for i in range(1,n1):      ###looping through the samples
+        #print(x1[i],i)
+        if x1[i]**2 + x2[i]**2 >= 3.5:    ####checking acc to the MC approach
+                count_mc = count_mc+1
+        c_m.append(count_mc / i)
+        x_m.append(i)
+print("MC",count_mc / n1)
+
+
+##########################MC+IS
+count_is = 0
+c_is = []
+x_is = []
+#wei = distr.pdf(sam) / distr_sam.pdf(sam)
+#print(wei)
+for i in range(1,n1):  #looping through the samples
+        if (sam_x1[i]**2 + sam_x2[i]**2 >= th):
+                k=(
+            (norm.pdf(sam_x1[i], loc=mean_x, scale=var_x) /
+             norm.pdf(sam_x1[i], loc=sam_mean_x, scale=sam_var_x))* (norm.pdf(sam_x2[i], loc=mean_x, scale=var_x) /
+             norm.pdf(sam_x2[i], loc=sam_mean_x, scale=sam_var_x)))
+                count_is = count_is +  k
+                #print(k)
+        c_is.append(count_is / i)
+        x_is.append(i)
+print("MC+IS", count_is / n1)
+
+
+
+true = []
+st = []
+for j in range(n1):
+    true.append(0.17377)
+    st.append(j)
+plt.figure()
+plt.plot(x1,x2,
+         'o',
+         c='lime',
+         markeredgewidth=0.5,
+         markeredgecolor='black')
+plt.plot(new_x1,new_x2,
+         'o',
+         c='red',
+         alpha=0.2,
+         markeredgewidth=0.5,
+         markeredgecolor='black')
+#plt.title(f'Green samples are X purple samples are Z')
+plt.xlabel('x1')
+plt.ylabel('x2')
+plt.axis('equal')
+plt.show()
+
+plt.figure()
+plt.grid()
+plt.plot(x_1, c1, label="IS new app")
+plt.plot(x_is, c_is, label="IS app")
+plt.plot(x_m, c_m, label="MC app")
+#plt.plot(st, true, label='True value')
+plt.plot(st, true, label='True value')
+plt.xlabel('Number of iterations')
+plt.legend()
+plt.show()
